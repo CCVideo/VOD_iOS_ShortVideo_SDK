880a81a56e05b440cd7e5692b9aea39ee18fee7f
Han Ruisong <550399108@qq.com>
2018-06-07 10:01:45 +0800

    更新到1.0.1

---
+++ ShortDemo/ShortVideoDemo/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  ShortVideoDemo
+//
+//  Created by luyang on 2017/7/28.
+//  Copyright © 2017年 Myself. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+
+@end
+
---
+++ ShortDemo/ShortVideoDemo/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  ShortVideoDemo
+//
+//  Created by luyang on 2017/7/31.
+//  Copyright © 2017年 Myself. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
